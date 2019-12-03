620bea9ddb234d3c4d5a2735cbd6846d93bad19b
Antoine Gagné <gagnantoine@gmail.com>
2019-12-02 21:17:00 -0500

    Start writing handmade parser

---
+++ src/ecureuil_parser.erl
@@ -0,0 +1,27 @@
+-module(ecureuil_parser).
+
+%% API
+-export([]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+parse(Raw) ->
+    case try_parse(Raw) of
+        {ok, Tree} -> {ok, flatten(Tree)};
+        Error -> Error
+    end.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+try_parse(Raw) ->
+    case ecureuil_lexer:string(Raw) of
+        {ok, Tokens, _} -> ecureuil_generated_parser:parse(Tokens);
+        Error -> Error
+    end.
+
+flatten(Value) ->
+    Value.
