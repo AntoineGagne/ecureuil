df4119603b5b01a5f8c6c67bae488f6fbe854c00
Antoine Gagné <gagnantoine@gmail.com>
2019-12-28 19:20:07 -0500

    WIP: Add by ids and by classes

--- src/ecureuil_html.erl
+++ src/ecureuil_html.erl
@@ -11,9 +11,21 @@
 
 -type attribute() :: {binary(), binary()}.
 -type html_node() :: {binary(), [attribute()], [html_node() | binary()]}.
+
+-type id() :: binary().
+-type class() :: binary().
+-type index() :: non_neg_integer().
+-type html_identifier() :: binary().
+-type by_ids() :: #{id() := [index()]}.
+-type by_classes() :: #{class() := [index()]}.
+-type by_indices() :: #{index() := {binary(), [attribute()], [index()]} | binary()}.
+-type by_identifiers() :: #{html_identifier() := [index()]}.
+
 -opaque html() :: #{tree := html_node(),
-                    by_ids := #{},
-                    by_identifiers := #{}}.
+                    by_ids := by_ids(),
+                    by_classes := by_classes(),
+                    by_indices := by_indices(),
+                    by_identifiers := by_identifiers()}.
 
 %%%===================================================================
 %%% API
@@ -26,7 +38,7 @@ parse(Raw) ->
         Indices = build_index(Parsed),
         {ok, Indices}
     catch
-        E:R -> {error, {E, R}}
+        E:R:S -> {error, {E, R, S}}
     end.
 
 -spec tree(html()) -> html_node().
@@ -48,36 +60,30 @@ attribute({_, Attributes, _}, Attribute) ->
 %%%===================================================================
 
 build_index(Parsed) ->
-    ByIds = build_tree_by_indices(Parsed),
-    ByIdentifiers = build_tree_by_identifiers(ByIds),
-    #{tree => Parsed, by_ids => ByIds, by_identifiers => ByIdentifiers}.
-    %{_, ByIds} = build_tree_by_indices2({0, []}, Parsed),
-    %#{tree => Parsed, by_ids => maps:from_list(ByIds)}.
+    ByIndices = build_tree_by_indices(Parsed),
+    ByIdentifiers = build_tree_by_identifiers(ByIndices),
+    ByClasses = build_tree_by_classes(ByIndices),
+    ByIds = build_tree_by_ids(ByIndices),
+    #{tree => Parsed,
+      by_indices => ByIndices,
+      by_identifiers => ByIdentifiers,
+      by_ids => ByIds,
+      by_classes => ByClasses}.
 
 build_tree_by_indices(Parsed) ->
-    {_, ByIds} = do_build_tree_by_indices2({0, []}, Parsed),
-    ByIds.
+    {_, ByIndices} = do_build_tree_by_indices({0, []}, Parsed),
+    maps:from_list(ByIndices).
 
-do_build_tree_by_indices2({Start, Index}, {Identifier, Attributes, Children}) ->
+do_build_tree_by_indices({Start, Index}, {Identifier, Attributes, Children}) ->
     Build = fun (Node, {I, Acc}) ->
-                    do_build_tree_by_indices2({I, Acc}, Node)
+                    do_build_tree_by_indices({I, Acc}, Node)
             end,
     {Start2, Index2} = lists:foldl(Build, {Start + 1, []}, Children),
     ChildrenIndices = lists:map(fun ({I, _}) -> I end, Index2),
     Index3 = [{Start, {Identifier, Attributes, ChildrenIndices}} | Index],
     {Start2, Index3 ++ Index2};
-do_build_tree_by_indices2({Start, Index}, Leaf) ->
-    {Start + 1, [{Start, Leaf} | Index]}.
-
-% do_build_tree_by_indices({Start, Index}, {Identifier, Attributes, Children}) ->
-%     Build = fun (Node, {I, Acc}) ->
-%                     do_build_tree_by_indices({I, Acc}, Node)
-%             end,
-%     {Start2, Index2} = lists:foldl(Build, {Start + 1, Index}, Children),
-%     Index3 = Index2#{Start => {Identifier, Attributes, lists:seq(Start + 1, Start2 - 1)}},
-%     {Start2, Index3};
-% do_build_tree_by_indices({Start, Index}, Leaf) ->
-%     {Start + 1, Index#{Start => Leaf}}.
+do_build_tree_by_indices({Start, Index}, Leaf) ->
+    {Start + 1, [{Start, {Leaf, [], []}} | Index]}.
 
 build_tree_by_identifiers(ByIds) ->
     U = fun (K, {Identifier, _, _}, A) ->
@@ -87,3 +93,37 @@ build_tree_by_identifiers(ByIds) ->
                 A
         end,
     maps:fold(U, #{}, ByIds).
+
+build_tree_by_classes(ByIndices) ->
+    U = fun (Index, Node, A) ->
+                Classes = classes(Node),
+                UpdateNodes = fun (Indices) -> [Index | Indices] end,
+                Update = fun (Id, Acc) -> maps:update_with(Id, UpdateNodes, [Index], Acc) end,
+                lists:foldl(Update, A, Classes)
+        end,
+    maps:fold(U, #{}, ByIndices).
+
+build_tree_by_ids(ByIndices) ->
+    U = fun (Index, Node, A) ->
+                Ids = ids(Node),
+                UpdateNodes = fun (Indices) -> [Index | Indices] end,
+                Update = fun (Id, Acc) -> maps:update_with(Id, UpdateNodes, [Index], Acc) end,
+                lists:foldl(Update, A, Ids)
+        end,
+    maps:fold(U, #{}, ByIndices).
+
+classes(Node) ->
+    case attribute(Node, <<"class">>) of
+        {error, _} -> [];
+        {ok, Raw} ->
+            Trimmed = string:trim(Raw),
+            string:lexemes(Trimmed, [$\t, $ ])
+    end.
+
+ids(Node) ->
+    case attribute(Node, <<"id">>) of
+        {error, _} -> [];
+        {ok, Raw} ->
+            Trimmed = string:trim(Raw),
+            string:lexemes(Trimmed, [$\t, $ ])
+    end.
